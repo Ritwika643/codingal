68963e8b9f079fa91095cbd5cbb4a48bf64926c2
Ritwika643 <ritwikakotturu@gmail.com>
2025-07-14 21:58:01 +0530

    functions

---
+++ lesson20/script.js
@@ -0,0 +1,14 @@
+function xyz() {
+  console.log("this is just a test");
+  console.log("this is just a demo");
+}
+xyz()
+xyz()
+function multiply(a,b,c,d){
+    var multiply = a * b * c * d;
+    return multiply
+}
+
+var result = multiply(2, 3, 4, 5);
+
+console.log(result);
