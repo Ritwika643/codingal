f029f4d811823b37688268649082c8e9da8b9fc4
Ritwika643 <ritwikakotturu@gmail.com>
2025-07-17 21:56:50 +0530

    methods,loops

---
+++ lesson21/script.js
@@ -0,0 +1,14 @@
+Math.random()
+
+var a = 'pqr'
+console.log(a.toUpperCase())
+console.log(a.startsWith("p"))
+
+var b = 79.75
+
+console.log(Math.floor(b))
+ 
+let i 
+for (i = 0; i < 10; i++) {
+    console.log("pqr")
+}
