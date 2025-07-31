89f5b09209cacb955ef554b31786a068a45c339f
Ritwika643 <ritwikakotturu@gmail.com>
2025-07-31 22:04:06 +0530

    DOM

---
+++ lesson24/script.js
@@ -0,0 +1,3 @@
+document.getElementById("coding").innerHTML = "Document";
+document.getElementsByClassName("class1")[0].innerHTML = "Codingal";
+document.getElementsByTagNameNS("p")[0].innerHTML = "This is my website";
