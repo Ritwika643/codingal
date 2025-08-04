d8f893f6c1864d903f4dfef103695a7b7c43fa47
Ritwika643 <ritwikakotturu@gmail.com>
2025-08-04 21:37:48 +0530

    belief

---
+++ lesson25/script.js
@@ -0,0 +1,47 @@
+console.log(a)
+
+var a = 24
+
+var b = 'hello world'
+
+let c = 'hi how are you?'
+
+let d = false
+
+let e = true
+
+let f = {age: 42, name: 'xyz'}
+
+let g = [4, 5, 5]
+
+c = 96
+
+c = 'ghi'
+
+console.log(c)
+
+i = 'idk'
+
+console.log(i)
+
+let j=0
+
+while(j < 20){
+
+console.log("pgq")
+
+j++
+
+}
+
+function add(a,b,c){
+
+return a + b + c
+
+}
+
+console.log(add(4,9,6))
+
+var add1 = (a,b,c) => a+b+c
+
+console.log(add1(4,9,6))
