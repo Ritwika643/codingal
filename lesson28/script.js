85b33b53ab6168ee877d800a486b73b372345fce
Ritwika643 <ritwikakotturu@gmail.com>
2025-08-14 21:57:01 +0530

    detail on js

---
+++ lesson28/script.js
@@ -0,0 +1,14 @@
+var a = 'hello world'
+var b = 25
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(String(b)))
+var c = 89
+console.log(typeof(Number(c)))
+
+try {
+    const i = 'coding';
+    i = 'javascript'
+} catch (error) {
+    console.log('handling your error' )
+}
