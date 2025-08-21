b6b920cdceb554e5c8b255ff4ca3aaf3bb047b0b
Ritwika643 <ritwikakotturu@gmail.com>
2025-08-21 21:58:31 +0530

    validation

---
+++ lesson30/script.js
@@ -0,0 +1,19 @@
+var a = document.getElementById("a");
+
+function validatecodingform() {
+    if (a [ 'fullname' ].value == abc ") {
+        alert("false user");
+        return false;
+    }
+    
+    if (a [ 'phonenumber' ].value == 123 ") {
+        alert("false user");
+        return false;
+    }
+    
+    if (a [ 'address' ].value == abc ") {
+        alert("false user");
+        return false;
+    }
+    
+   //code for successful validation
