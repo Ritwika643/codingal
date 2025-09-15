ee050062478334f4e0651c132feb91f36c1c6666
Ritwika643 <ritwikakotturu@gmail.com>
2025-09-15 22:05:26 +0530

    cal3

--- lesson34/script.js
+++ lesson34/script.js
@@ -18,5 +18,65 @@ numberButtons.forEach(b=> {
         updateDisplay(currentInput);
      }
     )}
+
 )
-   
+const calculate = (p, c, op) => {
+    switch(op) {
+        case 'addition':
+            return p + c;
+            case 'subtract':
+                return p - c;
+                case 'multiply':
+                    return p * c;
+                    case 'divide':
+                        return p / c;
+    }
+}
+
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+   const id = button.id;
+    switch(id) { 
+        case 'clear':
+            currentInput = '';
+            previousInput = '';
+            operator = null;
+            updateDisplay('');
+            break;
+        case 'backspace':
+            currentInput = currentInput.slice(0, -1);
+            updateDisplay(currentInput);
+        case 'divide':
+        case 'multiply':
+        case 'subtract':
+        case 'addition':
+            if (currentInput){
+                if (previousInput && operator) {
+                    const result = calculate(parseFloat(previousInput), 
+                   parseFloat(currentInput), operator);
+
+                   previousInput = result.tostring();
+                   updateDisplay(result);
+                }else {
+                    previousInput = currentInput;
+
+            }
+            currentInput = '';
+            operator = id;
+            }
+            break;
+        case 'equals':
+            if (previousInput && currentInput && operator) {
+                const result = calculate(parseFloat(previousInput),
+                parseFloat(currentInput), operator);
+                updateDisplay(result.toString());
+                currentInput = result.toString();
+                previousInput = '';
+                operator = null;
+            }
+            break;
+
+    }
+})
+})
