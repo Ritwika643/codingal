e6bf2766f318fad405e3ccbed9f8c21628f00ae1
Ritwika643 <ritwikakotturu@gmail.com>
2025-10-09 19:21:24 +0530

    armstrong

---
+++ lesson38/demo.py
@@ -0,0 +1,19 @@
+for i in range(4,44,4):
+    print(i)
+
+for i in range(0,10):
+    print(i)
+
+z = 24 
+counter = 0 
+while z == 24:
+    if counter == 200:
+        z = 50
+
+    print("hello")
+    counter = counter + 1
+
+
+for i in range(0,5):
+    for j in range(0,3):
+        print(f"i is {i} and j is {j}")
