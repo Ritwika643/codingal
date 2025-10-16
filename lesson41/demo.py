ad2e01663ffd039e193952130bce3cfb1c8a96b6
Ritwika643 <ritwikakotturu@gmail.com>
2025-10-16 21:59:28 +0530

    commit

---
+++ lesson41/demo.py
@@ -0,0 +1,29 @@
+abc = [1 , 56 , 23 , 78 , 3425 , 90 , 85 , 145 , 256 , 789]
+
+print(abc[5])
+
+xyz = ['zero', 'one', 'two' , 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+print(xyz[0])
+
+print (len(abc))
+
+xyz[0]= "hello"
+
+print(xyz[0])
+
+for i in range(0, len(xyz)):
+
+    print(xyz[i])
+
+yyyy = (False, True, True, True, False)
+
+aaa = {'name' : 'raj', 'age': 35, 'city': 'New York'}
+
+print(aaa['name'])
+
+print(len(aaa))
+
+aaa['age'] = 38
+
+print(aaa['age'])
