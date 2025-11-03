411ab1aba72ea6e53b17688924c7f02eaac3ea8a
Ritwika643 <ritwikakotturu@gmail.com>
2025-11-03 21:27:42 +0530

    obj orientated

---
+++ lesson44/demo.py
@@ -0,0 +1,9 @@
+class def__init__(self,name,age):
+    self.name=name
+    self.age=age
+    print("hello my name is",self.name)
+
+    def P(self):
+        print("hello")
+
+obj=def__init__("sachin",24)
