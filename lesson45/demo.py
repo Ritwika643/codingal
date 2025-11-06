6eca00890b838d8f2dd45a67b6fead97f47a2030
Ritwika643 <ritwikakotturu@gmail.com>
2025-11-06 21:53:30 +0530

    inheriitance

--- lesson45/demo.py
+++ lesson45/demo.py
@@ -16,3 +16,27 @@ class Student:
 s1=Student("Amit",80)
 print(s1.name)
 print(s1.marks)
+
+#Types of Inheritance
+
+#1.Single Inheritance
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+class Dog(Animal):
+    def bark(self):
+        print("Dog barks")
+
+#2. Multi-level Inheritance
+class Father:
+    def quality(self):
+        print("Honest and hardworking")
+class Mother:
+    def quality(self):
+        print("Caring and loving")
+class Child(Father, Mother):
+    def talent(self):
+        print("Singing and dancing")
+c=Child()
+c.quality()
+c.talent()
