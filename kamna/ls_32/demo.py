8ad1db6e8e729aaef6aae362442418ad7d7e6278
Ritwika643 <ritwikakotturu@gmail.com>
2026-03-18 14:37:48 +0530

    commit

--- kamna/ls_32/demo.py
+++ kamna/ls_32/demo.py
@@ -1,4 +1,4 @@
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 
 student_names = ["sanjay", "rahul", "karan" , "wasim" , "ramesh" , "ajay", 'sartaj']
 student_marks = [90, 80, 70, 60, 50, 40, 30] 
@@ -27,3 +27,14 @@ def percentage_bar_graph(student_names, marks_per):
     plt.title("Student Marks Bar Graph")
     plt.show()
 percentage_bar_graph()
+import matplotlib.pyplot as plt
+
+fruit_names= ['apple','mango','watermelon','dragonfruit','banana']
+fruit_cost=['250','520','300','110','320']
+
+print(fruit_cost)
+
+def cost_line_graph(cost, name):
+ plt.plot (cost , name)
+ plt.xlabel('cost of 1 fruit')
+ plt.ylabel('name of the fruit')
